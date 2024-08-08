5b9436fcb2b07effe1dc6e36bf9b9ab56d9cf3d1
singhtameshwar <159401216+singhtameshwar@users.noreply.github.com>
2024-08-08 15:47:11 +0530

    adding more functions

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,31 +7,26 @@ const Card = () => {
   const [inputSec, setInputSec] = useState("");
   const [sellToken, setSellToken] = useState("");
   const [buyToken, setBuyToken] = useState("");
-  const [price,setprice]=useState("");
-  console.log(price)
- 
-  useEffect(() => {
-    if(sellToken&&buyToken){
-      const url = ` https://api.0x.org/swap/v1/price?buyToken=${buyToken}&sellToken=${sellToken}&sellAmount=100000`;
-      axios
-        .get(url, {
-          headers: { "0x-api-key": "f8a0dc95-a1b2-424b-9f91-839ea88e3e43" },
-        })
-        .then(function (response) {
-          setprice(response.data.price);
-          console.log(response.data);
-        })
-        .catch(function (error) {
-          console.log("its my error:", error);
-        });
-    }
-}, [sellToken, buyToken]);
+  const [price, setPrice] = useState("");
 
-// useEffect(() => {
-//   if (price && inputOne) {
-//     setInputSec((price + inputOne));
-//   }
-// }, [price, inputOne]);
+  async function getPrice() {
+    if (sellToken && buyToken) {
+      const url = `https://api.0x.org/swap/v1/price?buyToken=${buyToken}&sellToken=${sellToken}&sellAmount=1000000000`;
+      console.log(url);
+      try {
+        const response = await axios.get(url, {
+          headers: { "0x-api-key": "f8a0dc95-a1b2-424b-9f91-839ea88e3e43" },
+        });
+        setPrice(response.data.price);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+
+  useEffect(() => {
+    getPrice();
+  }, [sellToken, buyToken, inputOne, inputSec]);
 
   const clearInputOne = () => {
     setInputOne("");
@@ -51,7 +46,6 @@ const Card = () => {
             <button className="btn btn-link decoration-black">CrossChain</button>
           </div>
         </div>
-
         <div className="container border-slate-300 border-y-2">
           <p className="mt-2 mb-2 text-xs">Sell</p>
           <div className="flex justify-between">
@@ -67,7 +61,7 @@ const Card = () => {
                 DAI
               </option>
               <option value="0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48">
-                 USD
+                USD
               </option>
             </select>
           </div>
@@ -77,24 +71,23 @@ const Card = () => {
             </button>
           </div>
         </div>
-
         <div className="container mt-2 mb-2">
           <input
             type="number"
+            value={inputOne}
             placeholder="0.0"
             className="input input-bordered input-secondary w-full max-w-xs"
-            value={inputOne}
             onChange={(e) => setInputOne(e.target.value)}
           />
         </div>
-
         <div className="container border-slate-300 border-y-2">
           <p className="mt-2 mb-2 text-xs">Buy</p>
           <div className="flex justify-between">
             <select
               className="select select-bordered w-full max-w-xs"
-              onChange={(e) => setSellToken(e.target.value)}>
-           <option value="">choose</option>
+              onChange={(e) => setBuyToken(e.target.value)}
+            >
+              <option value="">choose</option>
               <option value="0xdac17f958d2ee523a2206206994597c13d831ec7">
                 ETH
               </option>
@@ -102,7 +95,7 @@ const Card = () => {
                 DAI
               </option>
               <option value="0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48">
-                 USD
+                USD
               </option>
             </select>
           </div>
@@ -112,16 +105,13 @@ const Card = () => {
             </button>
           </div>
         </div>
-
         <input
-  type="number"
-  placeholder="0.0"
-  className="input input-bordered input-secondary w-full max-w-xs"
-  // value={inputSec}
-    />clear
-        <div className="card-actions">
-          <button className="btn btn-active btn-neutral">connectwallet</button>
-        </div>
+          type="number"
+          placeholder="0.0"
+          className="input input-bordered input-secondary w-full max-w-xs"
+          value={price}
+          readOnly
+        />
       </div>
     </div>
   );

31a6c6a63d12d7bc7e13cbad663de80346788c9a
singhtameshwar <159401216+singhtameshwar@users.noreply.github.com>
2024-08-21 17:10:12 +0530

    adding new function

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,6 +9,7 @@ const Card = () => {
   const [buyToken, setBuyToken] = useState("");
   const [price, setPrice] = useState("");
 
+
   async function getPrice() {
     if (sellToken && buyToken && inputOne) {
       const url = `https://api.0x.org/swap/v1/price?buyToken=${buyToken}&sellToken=${sellToken}&sellAmount=${inputOne}`;
@@ -21,7 +22,24 @@ const Card = () => {
         console.log(err);
       }
     }
-  };
+  }
+  async function swapTokens() {
+    if (sellToken && buyToken && inputOne) {
+      const url = `https://api.0x.org/swap/v1/quote?buyToken=${buyToken}&sellToken=${sellToken}&sellAmount=${inputOne}`;
+      try {
+        const response = await axios.get(url, {
+          headers: { "0x-api-key": "f8a0dc95-a1b2-424b-9f91-839ea88e3e43" },
+        });
+        const { data } = response;
+        console.log(data);
+        const { to, data: txData, value } = data;
+        console.log('Transaction data', { to, txData, value });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+
   useEffect(() => {
     getPrice();
   }, [sellToken, buyToken, inputOne, inputSec]);
@@ -30,12 +48,12 @@ const Card = () => {
     setInputOne("");
   };
 
-  const clearInputSec = () => {
-    setInputSec("");
-  };
+  useEffect(()=>{
+    swapTokens();
+  },[sellToken,buyToken,inputOne])
 
   return (
-    <div className="card bg-base-100 w-96 shadow-xl border-2 border-inherit">
+    <div className="card bg-base-100 max-w-full md:max-w-md shadow-xl border-2 border-inherit mx-auto my-4">
       <div className="card-body">
         <div className="container">
           <div className="flex justify-around">
@@ -44,11 +62,11 @@ const Card = () => {
             <button className="btn btn-link decoration-black">CrossChain</button>
           </div>
         </div>
-        <div className="container border-slate-300 border-y-2">
+        <div className="container border-slate-300 border-y-2 mt-4">
           <p className="mt-2 mb-2 text-xs">Sell</p>
           <div className="flex justify-between">
             <select
-              className="select select-bordered w-full max-w-xs"
+              className="select select-bordered w-full md:max-w-xs"
               onChange={(e) => setSellToken(e.target.value)}
             >
               <option value="">choose</option>
@@ -75,19 +93,19 @@ const Card = () => {
             min="0"
             value={inputOne}
             placeholder="0.0"
-            className="input input-bordered input-secondary w-full max-w-xs"
+            className="input input-bordered input-secondary w-full md:max-w-xs"
             onChange={(e) => setInputOne(e.target.value)}
           />
         </div>
-        <div className="container border-slate-300 border-y-2">
+        <div className="container border-slate-300 border-y-2 mt-4">
           <p className="mt-2 mb-2 text-xs">Buy</p>
           <div className="flex justify-between">
             <select
-              className="select select-bordered w-full max-w-xs"
+              className="select select-bordered w-full md:max-w-xs"
               onChange={(e) => setBuyToken(e.target.value)}
             >
               <option value="">choose</option>
-              <option value="0xdac17f958d2ee523a2206206994597c13d831ec7">
+              <option value="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee">
                 ETH
               </option>
               <option value="0x6b175474e89094c44da98b954eedeac495271d0f">
@@ -98,19 +116,17 @@ const Card = () => {
               </option>
             </select>
           </div>
-          <div>
-            <button className="btn btn-sm mt-3 mb-3" onClick={clearInputSec}>
-              clear
-            </button>
-          </div>
         </div>
         <input
           type="number"
           placeholder="0.0"
-          className="input input-bordered input-secondary w-full max-w-xs"
+          className="input input-bordered input-secondary w-full md:max-w-xs"
           value={price}
           readOnly
         />
+        <div>
+          <button className="btn btn-sm mt-3 mb-3"onClick={swapTokens}>SWAP Token</button>
+        </div>
       </div>
     </div>
   );
